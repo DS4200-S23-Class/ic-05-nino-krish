f5ad5fa84e8a3957ebcf6319b1eb0b69d1350975
krishmoran <43583683+krishmoran@users.noreply.github.com>
2023-01-30 15:51:25 -0500

    fixed var to let

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // get the button element, and store it in a variable
-var button = document.querySelector("#mybutton");
+let button = document.querySelector("#mybutton");
 
 // get the element that displays the number of clicks, and store it in a variable
-var clicks = document.querySelector("#clicks");
+let clicks = document.querySelector("#clicks");
 
 // set the initial number of clicks to 0
-var count = 0;
+let count = 0;
 
 // add an event listener to the button element
 button.addEventListener("click", function() { 
